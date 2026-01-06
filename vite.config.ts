062c765c3e654129b1b515ba1c36ce13db20bf39
Marcelino.35 <136177164+hbtmarc@users.noreply.github.com>
2026-01-06 12:32:53 -0300

    Correção

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
-﻿export default defineConfig({
-  base: "/pvault/",
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
   plugins: [react()],
 });
